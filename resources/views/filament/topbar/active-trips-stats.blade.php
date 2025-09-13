e14ba47f6eded551d206543391b950d5ac41a572
Mikail <mikadonline@gmail.com>
2025-09-13 19:36:55 +0100

    fix: remove bold formatting from active trips count display for consistency

--- resources/views/filament/topbar/active-trips-stats.blade.php
+++ resources/views/filament/topbar/active-trips-stats.blade.php
@@ -1,4 +1,4 @@
 <span class="text-sm">
     <x-heroicon-m-truck class="inline-block mr-1 align-middle" style="width: 20px !important; height: 20px !important; display: inline-block !important;" />
-    <b>{{ number_format($activeTripsCount) }}</b> {{ Str::plural('Active Trip', $activeTripsCount) }}
+    {{ number_format($activeTripsCount) }} {{ Str::plural('Active Trip', $activeTripsCount) }}
 </span>
